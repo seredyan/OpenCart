2ef04b5837e67afd1c25c63160cb7853b290c83a
Sergey Sereda <83193859+seredyan@users.noreply.github.com>
2022-02-15 13:06:21 -0800

    there was a fixture to get logged created

--- tests/test_users/test_edit_account.py
+++ tests/test_users/test_edit_account.py
@@ -13,14 +13,21 @@ def login_page(browser, config):
     return page
 
 
-def test_edit_users_lastname(login_page, db, browser, config):
+@pytest.fixture(scope="function")
+def get_selected_user(db, browser, config, login_page):
     login_page.check_available_users(db)
+    global old_users
     old_users = db.get_users_list()
     selected_user = random.choice(old_users)
-    index = old_users.index(selected_user)
+    return selected_user
+
+
+
+def test_edit_users_lastname(login_page, get_selected_user, db, browser, config):
+    index = old_users.index(get_selected_user)
     modified_user = User(lastname="NEW_LASTNAME_77777")
 
-    login_page.edit_account(selected_user.username, modified_user, password="test123")
+    login_page.edit_account(get_selected_user.username, modified_user, password="test123")
     new_users = db.get_users_list()
     old_users[index].lastname = modified_user.lastname
 
@@ -29,29 +36,22 @@ def test_edit_users_lastname(login_page, db, browser, config):
 
 
 
-def test_edit_users_firstname(login_page, db, browser, config):
-    login_page.check_available_users(db)
-    old_users = db.get_users_list()
-    selected_user = random.choice(old_users)
-
-    index = old_users.index(selected_user)
-
+def test_edit_users_firstname(login_page, get_selected_user, db, browser, config):
+    index = old_users.index(get_selected_user)
     modified_user = User(firstname="NEW_FIRSTNAME_EEEE")
-    login_page.edit_account(selected_user.username, modified_user, password="test123")
+
+    login_page.edit_account(get_selected_user.username, modified_user, password="test123")
     new_users = db.get_users_list()
     old_users[index].firstname = modified_user.firstname
 
     assert old_users == new_users
 
 
-def test_edit_users_phone_number(login_page, db, browser, config):
-    login_page.check_available_users(db)
-    old_users = db.get_users_list()
-    selected_user = random.choice(old_users)
-    index = old_users.index(selected_user)
+def test_edit_users_phone_number(login_page, get_selected_user, db, browser, config):
+    index = old_users.index(get_selected_user)
     modified_user = User(phone="777777")
 
-    login_page.edit_account(selected_user.username, modified_user, password="test123")
+    login_page.edit_account(get_selected_user.username, modified_user, password="test123")
     new_users = db.get_users_list()
     old_users[index].phone = modified_user.phone
 
@@ -59,16 +59,12 @@ def test_edit_users_phone_number(login_page, db, browser, config):
 
 
 
-def test_edit_users_username_account(login_page, db, browser, config):
-    login_page.check_available_users(db)
-    old_users = db.get_users_list()
-    selected_user = random.choice(old_users)
-
-    index = old_users.index(selected_user)
+def test_edit_users_username_account(login_page, get_selected_user, db, browser, config):
+    index = old_users.index(get_selected_user)
     edited_username = generator.users.random_char_email(6)
     modified_user = User(username=edited_username)
 
-    login_page.edit_account(selected_user.username, modified_user, password="test123")
+    login_page.edit_account(get_selected_user.username, modified_user, password="test123")
     new_users = db.get_users_list()
     old_users[index].username = modified_user.username
 
--- tests/test_users/test_users_add_to_cart_items.py
+++ tests/test_users/test_users_add_to_cart_items.py
@@ -8,6 +8,15 @@ from model.cart import Cart
 import random
 
 
+@pytest.fixture(scope="function")
+def user_login(db, browser, config, category_page):
+    login_page = LoginPage(browser, config)
+    login_page.check_available_users(db)
+    selected_user = random.choice(db.get_users_list())
+    login_page.login(selected_user.username, "test123")
+    return selected_user
+
+
 @pytest.fixture
 def category_page(browser, config):
     page = CategoryPage(browser, config)
@@ -18,12 +27,7 @@ def category_page(browser, config):
 
 
 @pytest.mark.parametrize('item', [num for num in range(43, 47)])
-def test_random_user_can_add_to_cart_random_item_from_category_page(db, category_page, browser, config, item):
-    login_page = LoginPage(browser, config)
-    login_page.check_available_users(db)
-
-    selected_user = random.choice(db.get_users_list())
-    login_page.login(selected_user.username, "test123")
+def test_random_user_can_add_to_cart_random_item_from_category_page(db, user_login, category_page, browser, config, item):
     category_page.open_laptops_page()
     page = ItemPage(browser, category_page.config)
     page.add_to_cart(item)
@@ -33,12 +37,7 @@ def test_random_user_can_add_to_cart_random_item_from_category_page(db, category
 
 
 @pytest.mark.parametrize('item', [num for num in range(43, 48)])
-def test_random_user_can_add_to_cart_random_item_from_opened_single_item_page(db, category_page, browser, config, item):
-    login_page = LoginPage(browser, config)
-    login_page.check_available_users(db)
-
-    selected_user = random.choice(db.get_users_list())
-    login_page.login(selected_user.username, "test123")
+def test_random_user_can_add_to_cart_random_item_from_opened_single_item_page(db, category_page, user_login, browser, config, item):
     category_page.open_laptops_page()
     page = ItemPage(browser, category_page.config)
     page.open_item_info_page(item)
@@ -49,18 +48,13 @@ def test_random_user_can_add_to_cart_random_item_from_opened_single_item_page(db
 
 
 
-def test_check_cart_after_user_added_to_cart_some_item(db, category_page, browser, config, item=45):
-    login_page = LoginPage(browser, config)
-    login_page.check_available_users(db)
-
-    selected_user = random.choice(db.get_users_list())
-    login_page.login(selected_user.username, "test123")
+def test_check_cart_after_user_added_to_cart_some_item(db, user_login, category_page, browser, config, item=45):
     old_cart = db.get_items_in_cart_list()
     category_page.open_laptops_page()
     page = ItemPage(browser, category_page.config)
     page.add_to_cart(item)
     new_cart = db.get_items_in_cart_list()
-    used_cart = Cart(customer=int(selected_user.id), product=item, qty=1)
+    used_cart = Cart(customer=int(user_login.id), product=item, qty=1)
     quantity = page.get_qty(used_cart, old_cart, new_cart)
 
     assert old_cart == new_cart
@@ -68,27 +62,6 @@ def test_check_cart_after_user_added_to_cart_some_item(db, category_page, browse
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def login_random_user(db, browser, config):
-#     page = LoginPage(browser, config)
-#     if page.is_logged_in():
-#         page.logout()
-#     old_users = db.get_users_list()
-#     selected_user = random.choice(old_users)
-#     page.login(selected_user.username, "test123")
-#     return selected_user
 
 
 
